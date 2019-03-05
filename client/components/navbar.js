9043a36cdb6078fe8efc788f8f842a40b5121f67
Tetiana Farhuts <tfarguts@gmail.com>
2019-03-04 22:19:26 -0500

    navbar-flex

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -9,19 +9,19 @@ const Navbar = ({handleClick, isLoggedIn}) => (
     <nav>
       <div className="nav-wraper">
         <div className="container">
-          <ul className="orders">
-            <Link to="/orders" className="btn-floating z-depth-0 white right">
-              <i className="material-icons pink-text darken-1">
-                add_shopping_cart
-              </i>
-            </Link>
-          </ul>
+          <Link to="/orders" className="btn-floating z-depth-0 white right">
+            <i className="material-icons pink-text darken-1">
+              add_shopping_cart
+            </i>
+          </Link>
           <Link to="/products" className="right">
             All Mittens
           </Link>
-          <Link to="/aboutUs" className="brand-logo">
-            <i className="large material-icons"> pets </i>Mittens
-          </Link>
+          <ul className="hide-on-med-and-down">
+            <Link to="/aboutUs" className="brand-logo">
+              <i className="large material-icons"> pets </i>Mittens
+            </Link>
+          </ul>
           <div className="right">
             {isLoggedIn ? (
               <div>
