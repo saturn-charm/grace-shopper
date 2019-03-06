37b531ad4beba7c89cc3f14790bf5a547dc0d1fa
Stephanie Coy <scoy028@gmail.com>
2019-03-05 19:09:28 -0500

    adding protection to order, user, and itemsInCart route

--- client/store/order.js
+++ client/store/order.js
@@ -33,9 +33,6 @@ export const getUserOrderThunk = () => {
     try {
       const orderResponse = await axios.get('/api/orders/myCart')
       const existingOrder = orderResponse.data
-      const itemsInOrderResponse = await axios.get(
-        `/api/orders/myCart/${existingOrder.id}`
-      )
       if (!existingOrder.guestCart || !existingOrder.itemsInOrder) {
         //if there is no guestcart on existingOrder, meaning we are either logged in or haven't initialized a cart
         const itemsInOrderResponse = await axios.get(
--- server/api/users.js
+++ server/api/users.js
@@ -2,17 +2,25 @@ const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+const makeError = (status, message) => {
+  const err = new Error(message)
+  err.status = status
+  return err
+}
+const isAdmin = (req, res, next) => {
+  // let isAdmin = req.user.admin ? true : false ;
+  let isAdmin = req.user ? req.user.admin : false
+  if (!isAdmin) return next(makeError(403, 'Forbidden'))
+  next()
+}
+
+router.get('/', isAdmin, async (req, res, next) => {
   try {
-    // console.log(
-    //   'req.session.passport.user in order get route: ',
-    //   req.session.passport.user
-    // )
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
       // users' passwords are encrypted, it won't help if we just
       // send everything to anyone who asks!
-      attributes: ['id', 'email']
+      attributes: ['id', 'email', 'admin']
     })
     res.json(users)
   } catch (err) {
