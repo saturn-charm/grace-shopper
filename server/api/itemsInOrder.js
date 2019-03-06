bde06bb9cfecfdfd4429be3b0cf12de244364573
Stephanie <36521666+scoy028@users.noreply.github.com>
2019-03-05 19:37:25 -0500

    Route protection (#64)
    * order-functionality-prettification
    
    * order-checkout-btn
    
    * user-home component, added a btn
    
    * navbar-flex
    
    * added text indicating that items were added to the card
    
    * Update client/components/order.js
    
    Co-Authored-By: Farhuts <tfarguts@gmail.com>
    
    * Update client/components/product-details.js
    
    Co-Authored-By: Farhuts <tfarguts@gmail.com>
    
    * Update client/components/user-home.js
    
    Co-Authored-By: Farhuts <tfarguts@gmail.com>
    
    * adding protection to order, user, and itemsInCart route
    
    * travis
    
    * delete user test

--- server/api/itemsInOrder.js
+++ server/api/itemsInOrder.js
@@ -1,8 +1,20 @@
 const router = require('express').Router()
 const {ItemInOrder} = require('../db/models')
 
+const makeError = (status, message) => {
+  const err = new Error(message)
+  err.status = status
+  return err
+}
+const isAdmin = (req, res, next) => {
+  // let isAdmin = req.user.admin ? true : false ;
+  let isAdmin = req.user ? req.user.admin : false
+  if (!isAdmin) return next(makeError(403, 'Forbidden'))
+  next()
+}
+
 // api/itemsInOrder/:productId
-router.get('/:productId', async (req, res, next) => {
+router.get('/:productId', isAdmin, async (req, res, next) => {
   try {
     const productId = req.params.productId
     const response = await ItemInOrder.findById(productId)
